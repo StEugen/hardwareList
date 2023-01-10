15b30a236a844e0f6e8b120f7a75051201a754b8
StEugen <85479518+StEugen@users.noreply.github.com>
2023-01-11 03:14:57 +0400

    update

--- hardware-list/hardwareList/settings.py
+++ hardware-list/hardwareList/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'hardware',
         'USER': 'hard',
         'PASSWORD': 'PASSWORD',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': ''
     }
 }
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS=['http://192.168.0.106']
